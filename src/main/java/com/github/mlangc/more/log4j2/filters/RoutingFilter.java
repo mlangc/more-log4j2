24b73d8a0f3335bcaf8b35eeeeda3109e2d6d8e0
Matthias Langer <m.langer798@gmail.com>
2025-10-21 22:01:30 +0200

    Add license

--- src/main/java/com/github/mlangc/more/log4j2/filters/RoutingFilter.java
+++ src/main/java/com/github/mlangc/more/log4j2/filters/RoutingFilter.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * more-log4j2
+ * %%
+ * Copyright (C) 2025 Matthias Langer
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.github.mlangc.more.log4j2.filters;
 
 
@@ -14,10 +33,8 @@ import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.message.Message;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.Objects.requireNonNullElse;
 
 @Plugin(name = "RoutingFilter", category = Node.CATEGORY, elementType = Filter.ELEMENT_TYPE, printObject = true)
 public class RoutingFilter extends AbstractLifeCycle implements Filter {
