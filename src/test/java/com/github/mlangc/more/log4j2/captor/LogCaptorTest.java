22d8b8ac91c1d42c0268e07212ed3df6a0b32d2d
Matthias Langer <m.langer798@gmail.com>
2026-01-14 22:43:11 +0100

    More tests

--- src/test/java/com/github/mlangc/more/log4j2/captor/LogCaptorTest.java
+++ src/test/java/com/github/mlangc/more/log4j2/captor/LogCaptorTest.java
@@ -33,4 +33,79 @@ class LogCaptorTest {
         LOG.info("should not be suppressed");
         assertThat(logCaptor.getInfoLogs()).hasSize(1);
     }
+
+   @Test
+   void shouldCaptureDebugLogs() {
+       logCaptor.setLogLevelToDebug();
+       LOG.debug("debug message");
+       assertThat(logCaptor.getDebugLogs()).containsExactly("debug message");
+   }
+
+   @Test
+   void shouldCaptureWarnLogs() {
+       LOG.warn("warning message");
+       assertThat(logCaptor.getWarnLogs()).containsExactly("warning message");
+   }
+
+   @Test
+   void shouldCaptureMultipleLogsAtSameLevel() {
+       LOG.info("first");
+       LOG.info("second");
+       LOG.info("third");
+       assertThat(logCaptor.getInfoLogs()).containsExactly("first", "second", "third");
+   }
+
+   @Test
+   void shouldCaptureAllLogsRegardlessOfLevel() {
+       logCaptor.setLogLevelToDebug();
+       LOG.debug("debug");
+       LOG.info("info");
+       LOG.warn("warn");
+       assertThat(logCaptor.getLogs()).containsExactly("debug", "info", "warn");
+   }
+
+   @Test
+   void shouldClearLogsSuccessfully() {
+       LOG.info("before clear");
+       logCaptor.clearLogs();
+       LOG.info("after clear");
+       assertThat(logCaptor.getInfoLogs()).containsExactly("after clear");
+   }
+
+   @Test
+   void shouldDisableLogsCompletely() {
+       logCaptor.disableLogs();
+       LOG.error("should not appear");
+       assertThat(logCaptor.getLogs()).isEmpty();
+   }
+
+   @Test
+   void shouldGetLogEventsWithMetadata() {
+       LOG.info("test message");
+       assertThat(logCaptor.getLogEvents())
+           .hasSize(1)
+           .allSatisfy(event -> {
+               assertThat(event.getMessage().getFormattedMessage()).isEqualTo("test message");
+               assertThat(event.getLevel()).isEqualTo(Level.INFO);
+               assertThat(event.getLoggerName()).isEqualTo(LOG.getName());
+           });
+   }
+
+   @Test
+   void shouldFilterLogsByLevel() {
+       logCaptor.setLogLevelToDebug();
+       LOG.debug("debug");
+       LOG.info("info");
+       LOG.warn("warn");
+       assertThat(logCaptor.getLogs(Level.DEBUG)).containsExactly("debug");
+       assertThat(logCaptor.getLogs(Level.INFO)).containsExactly("info");
+       assertThat(logCaptor.getLogs(Level.WARN)).containsExactly("warn");
+   }
+
+   @Test
+   void shouldNotCaptureLogsFromOtherLoggers() {
+       Logger otherLogger = LogManager.getLogger("other.logger");
+       otherLogger.info("from other logger");
+       assertThat(logCaptor.getLogs()).isEmpty();
+   }
 }
